fca7918b19cbd4be7f27ba284d9a5cef775e565c
anjalikavadeaiden <anjali.kavade-i@aidenai.com>
2025-11-21 12:54:57 +0530

    Added dynaconf and uv to the project

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,15 @@
-from pydantic_settings import BaseSettings
+# app/core/config.py
+import os
+from dynaconf import Dynaconf
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-class Settings(BaseSettings):
-    DATABASE_URL: str
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
+settings = Dynaconf(
+    root_path=BASE_DIR,
+    settings_files=["settings.toml", ".secrets.toml"],
+    environments=True,
+    env_switcher="ENV_FOR_DYNACONF",
+    load_dotenv=True,
+    merge_enabled=True,
+    env="development",
+)
